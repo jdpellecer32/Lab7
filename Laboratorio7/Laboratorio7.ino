39bed25c32ab2f4c94a64abeafe14b0c0ab991ff
jdpellecer32 <44791358+jdpellecer32@users.noreply.github.com>
2020-03-26 15:08:23 -0600

    Parte 2
    Se muestra uno de los archivos de la SD

--- Laboratorio7/Laboratorio7.ino
+++ Laboratorio7/Laboratorio7.ino
@@ -2,13 +2,16 @@
 #include <SPI.h>
 #include <SD.h>
 
-File root;
+File myFile;
 
 void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   SPI.setModule(0);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
 
 
   Serial.print("Initializing SD card...");
@@ -18,44 +21,30 @@ void setup()
   // or the SD library functions will not work.
   pinMode(10, OUTPUT);
 
-  if (!SD.begin(32)) {
+  if (!SD.begin(4)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
 
-  root = SD.open("/");
+  // re-open the file for reading:
+  myFile = SD.open("GOMBAT.txt");
+  if (myFile) {
+    Serial.println("GOMBAT.txt:");
 
-  printDirectory(root, 0);
-
-  Serial.println("done!");
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
 
 void loop()
 {
-  // nothing happens after setup finishes.
-}
-
-void printDirectory(File dir, int numTabs) {
-   while(true) {
-     
-     File entry =  dir.openNextFile();
-     if (! entry) {
-       // no more files
-       break;
-     }
-     for (uint8_t i=0; i<numTabs; i++) {
-       Serial.print('\t');
-     }
-     Serial.print(entry.name());
-     if (entry.isDirectory()) {
-       Serial.println("/");
-       printDirectory(entry, numTabs+1);
-     } else {
-       // files have sizes, directories do not
-       Serial.print("\t\t");
-       Serial.println(entry.size(), DEC);
-     }
-     entry.close();
-   }
+  // nothing happens after setup
 }
