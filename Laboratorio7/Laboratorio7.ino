0f439af4acdff6d92fcdd3e99221b2fda8fbf8fb
jdpellecer32 <44791358+jdpellecer32@users.noreply.github.com>
2020-03-26 15:11:39 -0600

    Version final
    Se unió la parte 1 y la 2 en un solo codigo y se solucionó el error de mostrar el archivo 2 veces.

--- Laboratorio7/Laboratorio7.ino
+++ Laboratorio7/Laboratorio7.ino
@@ -2,6 +2,7 @@
 #include <SD.h>
 
 File myFile;
+File root;
 
 void setup()
 {
@@ -25,6 +26,12 @@ void setup()
     return;
   }
   Serial.println("initialization done.");
+
+  root = SD.open("/");
+  printDirectory(root, 0);
+  Serial.println("done!");
+
+
   Serial.println("Ingrese el numero 1 para escoger la opcion de Gombat");
   Serial.println("Ingrese el numero 2 para escoger la opcion de Luigi");
   Serial.println("Ingrese el numero 3 para escoger la opcion de MasterChief");
@@ -41,77 +48,115 @@ void loop()
     archivo = Serial.read();
     if (archivo == '1') {
       Serial.println("Escogio el archivo Gombat");
-      opcion=1;
+      opcion = 1;
     } else if (archivo == '2') {
       Serial.println("Escogio el archivo Luigi");
-      opcion=2;
+      opcion = 2;
     } else if (archivo == '3') {
       Serial.println("Escogio el archivo MasterChief");
-      opcion=3;
+      opcion = 3;
     } else {
       Serial.println("No ingreso ninguna opcion correcta");
       //mostrar = 0;
-     
+      opcion = 4;
+
     }
 
   }
   if (mostrar == 1) {
     switch (opcion) {
       case 1:
-        myFile = SD.open("GOMBAT.txt");
-        if (myFile) {
-          Serial.println("GOMBAT.txt:");
-
-          // read from the file until there's nothing else in it:
-          while (myFile.available()) {
-            Serial.write(myFile.read());
-          }
-          // close the file:
-          myFile.close();
-        } else {
-          // if the file didn't open, print an error:
-          Serial.println("error opening test.txt");
-        }
-        mostrar = 0;
+        mostrar_gombat();
         break;
       case 2:
-        myFile = SD.open("LUIGI.txt");
-        if (myFile) {
-          Serial.println("LUIGI.txt:");
-
-          // read from the file until there's nothing else in it:
-          while (myFile.available()) {
-            Serial.write(myFile.read());
-          }
-          // close the file:
-          myFile.close();
-        } else {
-          // if the file didn't open, print an error:
-          Serial.println("error opening test.txt");
-        }
-        mostrar = 0;
+        mostrar_luigi();
         break;
       case 3:
-        myFile = SD.open("MASTERCHIEF.txt");
-        if (myFile) {
-          Serial.println("MASTERCHIEF.txt:");
-
-          // read from the file until there's nothing else in it:
-          while (myFile.available()) {
-            Serial.write(myFile.read());
-          }
-          // close the file:
-          myFile.close();
-        } else {
-          // if the file didn't open, print an error:
-          Serial.println("error opening test.txt");
-        }
-        mostrar = 0;
+        mostrar_master();
         break;
-
-
+      case 4:
+        Serial.println("Por favor ingrese una opcion valida");
+        break;
     }
+    mostrar = 0;
+    Serial.end();
+    delay(1000);
+    Serial.begin(9600);
 
   }
 
+}
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
+}
+
+void mostrar_gombat() {
+  myFile = SD.open("GOMBAT.txt");
+  if (myFile) {
+    Serial.println("GOMBAT.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void mostrar_luigi() {
+  myFile = SD.open("LUIGI.txt");
+  if (myFile) {
+    Serial.println("LUIGI.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+void mostrar_master() {
+  myFile = SD.open("MASTER.txt");
+  if (myFile) {
+    Serial.println("MASTER.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
